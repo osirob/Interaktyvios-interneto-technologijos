5747a96409990f74a2b094dd7fec8d5843e4489e
tehFruit <76218849+tehFruit@users.noreply.github.com>
2022-05-16 20:32:25 +0300

    Adding multiple pictures works

--- Triperis/Controllers/ImagesController.cs
+++ Triperis/Controllers/ImagesController.cs
@@ -19,31 +19,35 @@ namespace Triperis.Controllers
         [Route("Upload/{id}")]
         public async Task<IActionResult> UploadImage([FromRoute] int id)
         {
-            var file = Request.Form.Files[0]; //i guess i dont index this if i want o upload more than 1 image?
-            
-            if(file.Length > 0)
+            var files = Request.Form.Files; //i guess i dont index this if i want o upload more than 1 image?
+
+            if (files.Count > 0)
             {
                 var savePath = Path.Combine(Directory.GetCurrentDirectory(), "Images");
-                var fileName =  Guid.NewGuid().ToString() + ".png";
-
-                var fullPath = Path.Combine(savePath, fileName);
-                var dbPath = Path.Combine("Images", fileName);
-
-                using (var stream = new FileStream(fullPath, FileMode.Create))
+                
+                for(int i = 0; i < files.Count; i++)
                 {
-                    file.CopyTo(stream);
+                    var fileName = Guid.NewGuid().ToString() + ".png";
+
+                    var fullPath = Path.Combine(savePath, fileName);
+                    var dbPath = Path.Combine("Images", fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        files[i].CopyTo(stream);
+                    }
+
+                    var imageDb = new Image()
+                    {
+                        Path = dbPath,
+                        CarId = id
+                    };
+
+                    dbContext.Images.Add(imageDb);
+                    await dbContext.SaveChangesAsync();
                 }
 
-                var imageDb = new Image()
-                {
-                    Path = dbPath,
-                    CarId = id
-                };
-
-                //dbContext.Images.Add(imageDb);
-                //await dbContext.SaveChangesAsync();
-
-                return Ok(new { dbPath });
+                return Ok();
             }
             else
             {
--- Triperis_Angular/Triperis/src/app/components/images/image-upload/image-upload.component.ts
+++ Triperis_Angular/Triperis/src/app/components/images/image-upload/image-upload.component.ts
@@ -10,7 +10,7 @@ import { HttpEventType } from '@angular/common/http';
 export class ImageUploadComponent implements OnInit {
   public message: string;
   public loading: boolean;
-  files : File[];
+  files : File[] = [];
   @Output() public onUploadFinished = new EventEmitter();
 
   constructor(private imagesService : ImagesService) { }
@@ -23,9 +23,20 @@ export class ImageUploadComponent implements OnInit {
       return;
     }
     this.loading = !this.loading;
-    let fileToUpload = <File>this.files[0];
-    const formData = new FormData();
-    formData.append('file', fileToUpload);
+    let filesToUpload = <File[]>this.files;
+  
+    //let fileToUpload = <File>this.files[0];
+    var formData = new FormData();
+
+    let index: Number = 0;
+
+    for(let file of filesToUpload){
+      formData.append('files', file);
+    }
+    
+    console.log(formData.getAll('files'));
+
+    //formData.append('file', filesToUpload);
     this.imagesService.uploadImage(formData, 1).subscribe( response =>{
       if(response.type === HttpEventType.Response){
         console.log('IT WORKED HAAAAAAAAAAAAAAAAAAAAAAAAAAA');
